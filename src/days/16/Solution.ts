692683d1462f4f1d3d09d79ae197b0cc5732885a
Jonathan Schmitz <jomitz96@hotmail.com>
2022-12-16 21:21:03 +0100

    Improve solution for Day 16

--- src/days/16/Solution.ts
+++ src/days/16/Solution.ts
@@ -1,8 +1,9 @@
 import {AbstractSolution} from "../../types/AbstractSolution";
 
-export class Solution extends AbstractSolution {
+type DistanceMap = { [fromValve: string]: { [toValve: string]: number } }
+type ValveMap = { [valve: string]: number }
 
-    distances: { [key: string]: { [key2: string]: number } } = {}
+export class Solution extends AbstractSolution {
 
     getFirstExampleSolution(): string {
         return "1651";
@@ -13,96 +14,75 @@ export class Solution extends AbstractSolution {
     }
 
     solveFirst(input: string): string {
-        // Read in values
-        let inputList = input.parseRows()
-            .map(row => row.split(new RegExp("; (?:tunnels lead|tunnel leads) to (?:valve|valves) ")))
-            .map(splitRow => [splitRow[0].substring(6, 8), splitRow[0].substring(23), splitRow[1]])
-
-        for (let inp of inputList) {
-            this.distances[inp[0]] = {}
-            this.distances[inp[0]][inp[0]] = 0
-
-            for (let neighbor of inp[2].split(", ")) {
-                this.distances[inp[0]][neighbor] = 1
-            }
-        }
-
-        // Read in Map of non-null valves
-        let valves: string[] = inputList
-            .map(row => row[0])
-
-        let valveMap: { [key: string]: number } = {}
-        for (let value of inputList) {
-            valveMap[value[0]] = parseInt(value[1])
-        }
-
-        let nonNullValves: string[] = inputList
-            .filter(row => parseInt(row[1]) !== 0)
-            .map(row => row[0])
-
-
-        // Construct map of the shortest paths between those
-        for (let nodeI of valves) {
-            for (let nodeJ of valves) {
-                for (let nodeK of valves) {
-                    this.distances[nodeI][nodeJ] = Math.min(this.distances[nodeI][nodeJ] ?? 99, (this.distances[nodeI][nodeK] ?? 99) + (this.distances[nodeK][nodeJ] ?? 99))
-                }
-            }
-        }
-
-        return this.calculatePressureLoss("AA", 30, nonNullValves, valveMap, false).toString()
+        let {distances, valveMap, nonNullValves} = this.constructValveMap(input);
+        return this.getMaxPressureLoss("AA", 30, distances, nonNullValves, valveMap, false).toString()
     }
 
     solveSecond(input: string): string {
+        let {distances, valveMap, nonNullValves} = this.constructValveMap(input);
+        return this.getMaxPressureLoss("AA", 26, distances, nonNullValves, valveMap, true).toString()
+    }
+
+    private constructValveMap(input: string) {
         // Read in values
         let inputList = input.parseRows()
-            .map(row => row.split(new RegExp("; (?:tunnels lead|tunnel leads) to (?:valve|valves) ")))
+            .map(row => row.split(/; (?:tunnels lead|tunnel leads) to (?:valve|valves) /))
             .map(splitRow => [splitRow[0].substring(6, 8), splitRow[0].substring(23), splitRow[1]])
 
-        for (let inp of inputList) {
-            this.distances[inp[0]] = {}
-            this.distances[inp[0]][inp[0]] = 0
-
-            for (let neighbor of inp[2].split(", ")) {
-                this.distances[inp[0]][neighbor] = 1
-            }
-        }
-
-        // Read in Map of non-null valves
+        // Generate a list of all valves
         let valves: string[] = inputList
             .map(row => row[0])
 
-        let valveMap: { [key: string]: number } = {}
-        for (let value of inputList) {
-            valveMap[value[0]] = parseInt(value[1])
+        // Build shortest distances Map for all valves
+        let distances: DistanceMap = {}
+
+        for (let inp of inputList) {
+            distances[inp[0]] = {}
+            distances[inp[0]][inp[0]] = 0
+
+            for (let neighbor of inp[2].split(", ")) {
+                distances[inp[0]][neighbor] = 1
+            }
         }
 
-        let nonNullValves: string[] = inputList
-            .filter(row => parseInt(row[1]) !== 0)
-            .map(row => row[0])
-
-
-        // Construct map of the shortest paths between those
-        for (let nodeI of valves) {
-            for (let nodeJ of valves) {
-                for (let nodeK of valves) {
-                    this.distances[nodeI][nodeJ] = Math.min(this.distances[nodeI][nodeJ] ?? 99, (this.distances[nodeI][nodeK] ?? 99) + (this.distances[nodeK][nodeJ] ?? 99))
+        // Construct map of the shortest paths between all valves
+        for (let fromValve of valves) {
+            for (let toValve of valves) {
+                for (let betweenValve of valves) {
+                    distances[fromValve][toValve] = Math.min(
+                        distances[fromValve][toValve] ?? 99,
+                        (distances[fromValve][betweenValve] ?? 99) + (distances[betweenValve][toValve] ?? 99)
+                    )
                 }
             }
         }
 
-        return this.calculatePressureLoss("AA", 26, nonNullValves, valveMap, true).toString()
+        // Set up map which maps valve to pressure loss
+        let valveMap: { [key: string]: number } = {}
+        for (let valveTuple of inputList) {
+            valveMap[valveTuple[0]] = parseInt(valveTuple[1])
+        }
+
+        // Read in Map of non-null valves
+        let nonNullValves: string[] = inputList
+            .filter(row => parseInt(row[1]) !== 0)
+            .map(row => row[0])
+
+        return {distances, valveMap, nonNullValves};
     }
 
-    calculatePressureLoss(i: string, t: number, remaining: string[], valveMap: {[key: string]: number}, hasHelp: boolean): number {
+    getMaxPressureLoss(currentValve: string, time: number, distances: DistanceMap, remainingValves: string[], valveToOutlet: ValveMap, hasHelp: boolean): number {
         // The initial result will only come through once. So always act as if the elephant now has to deal with the rest
-        // The elephant starts in "AA" EVERY TIME!!
-        let result = hasHelp ? this.calculatePressureLoss("AA", 26, remaining, valveMap, false) : 0
-        for (let j of remaining) {
-            let nextT = t - this.distances[i][j] - 1
-            if (nextT >= 0) {
-                let nextRemaining = remaining.filter(el => el !== j)
-                result = Math.max(result, valveMap[j] * nextT + this.calculatePressureLoss(j, nextT, nextRemaining, valveMap, hasHelp))
+        // The elephant starts in "AA" every time as well!!
+        let result = hasHelp ? this.getMaxPressureLoss("AA", 26, distances, remainingValves, valveToOutlet, false) : 0
+        for (let nextValve of remainingValves) {
+            let nextTime = time - distances[currentValve][nextValve] - 1
+            if (nextTime >= 0) {
+                let nextRemainingValves = remainingValves.filter(valve => valve !== nextValve)
+                result = Math.max(
+                    result,
+                    valveToOutlet[nextValve] * nextTime + this.getMaxPressureLoss(nextValve, nextTime, distances, nextRemainingValves, valveToOutlet, hasHelp)
+                )
             }
         }
 
